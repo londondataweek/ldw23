9dc27f8f5167f721060c24642a7a0efd5bd778e8
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-12 19:27:19 +0100

    Updated image size

--- src/sections/body/About.tsx
+++ src/sections/body/About.tsx
@@ -35,7 +35,7 @@ export default function About() {
             <span className=" dark:text-lightgreen text-darkgreentxt font-bold"> next pair of trainers</span>.
           </p>
           <img
-            className="dark:block hidden max-w-[20rem] h-48 w-fit md:h-60 mx-auto "
+            className="dark:block hidden max-w-[20rem] w-full h-auto xs:h-48 xs:w-fit md:h-60 mx-auto "
             role="presentation"
             src="./illustrations/LDW-Data-Icons-Transparent_8Crop.svg"
             alt="corporate-logos"
