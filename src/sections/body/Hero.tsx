c2f5b3a51715a0b8d0a021b1804f94a56d2fc832
Jennifer Ding <jending12@gmail.com>
2024-02-09 18:43:28 +0000

    add button and airtable

--- src/sections/body/Hero.tsx
+++ src/sections/body/Hero.tsx
@@ -2,8 +2,7 @@ import { useEffect } from 'react';
 import DownArrowSvg from '../../assets/icons/DownArrowSvg';
 import useIntersectionObserver from '../../utilities/hooks/useIntersectionObserver';
 import { useIntersectionProviderContext } from '../../utilities/contexts/IntersectionProvider';
-// import AnimatedLogo from '../../assets/logos/AnimatedLogo';
-import AnimatedLogo from '../../assets/logos/FullLogoWhiteColour2024';
+import AnimatedLogo from '../../assets/logos/AnimatedLogo_2024';
 import FullLogoWhiteColour from '../../assets/logos/FullLogoWhiteColour2024';
 
 export default function Hero() {
@@ -32,17 +31,26 @@ export default function Hero() {
           {!hasScrolled ? <AnimatedLogo /> : <FullLogoWhiteColour />}
         </div>
 
-        <div className=" row-start-3 m-auto grid gap-4 md:gap-16 text-center w-full fade-in">
-          <p className=" text-2xl xs:text-3xl md:text-5xl mx-auto w-fit font-bold ">1-7 July 2024</p>
-          <div className="text-2xl xs:text-3xl mx-auto text-center w-fit justify-center md:text-5xl flex flex-row flex-wrap gap-2">
+        <div className=" row-start-3 m-auto grid gap-4 md:gap-8 text-center w-full fade-in">
+          <p className=" text-2xl xs:text-3xl md:text-4xl mx-auto w-fit ">1-7 July 2024</p>
+          <div className="text-2xl xs:text-3xl mx-auto text-center w-fit justify-left md:text-4xl flex flex-row flex-wrap gap-2">
             <p className="w-fit text-center ">
-              <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold ">Data</span> in the
-              <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold "> public</span>,{' '}
+              <span className="dark:text-lightgreen text-center text-darkgreentxt ">Data</span> in the
+              <span className="dark:text-lightgreen text-center text-darkgreentxt "> public</span>,{' '}
             </p>
             <p className="w-fit text-center ">
-              for the<span className=" dark:text-lightgreen text-darkgreentxt font-bold "> public</span>.
+              for the<span className=" dark:text-lightgreen text-darkgreentxt "> public</span>.
             </p>
           </div>
+
+          <form>
+            <button formaction="https://airtable.com/appzyMC4CJl7GwcK5/shrRXtM6RidqKM6qu"
+              className="my-6 rounded-full w-60 flex flex-row items-center justify-center gap-2 h-12 text-lg sm:text-xl mx-auto px-4 py-2 font-bold text-darkblue dark:text-darkblue bg-darkpink dark:bg-pink hover:transition focus:transition hover:text-white hover:bg-darkblue  focus:text-white focus:bg-darkblue  dark:hover:bg-white  dark:focus:bg-white"
+            >
+              Get involved
+            </button>
+          </form>
+
         </div>
         <div className=" row-start-5 mb-auto h-fit sm:pb-8">
           <a
