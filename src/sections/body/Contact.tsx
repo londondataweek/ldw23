7b9a9a889847dae4f4feb36a059a010b88ec7aad
Jennifer Ding <jending12@gmail.com>
2024-02-19 19:41:49 +0000

    add links

--- src/sections/body/Contact.tsx
+++ src/sections/body/Contact.tsx
@@ -29,9 +29,9 @@ export default function Contact() {
         <h2 className="text-3xl sm:text-4xl font-bold w-fit mx-auto text-center ">Get Involved</h2>
 
         <p className=" text-2xl xs:text-2xl md:text-2xl mx-auto w-fit ">
-        To get involved with London Data Week 2024, fill out our
-        <a href="https://airtable.com/appzyMC4CJl7GwcK5/shrRXtM6RidqKM6qu"> <u>call for participation form</u> </a>
-         to apply to host an event or volunteer!
+        To learn more about how to get involved with London Data Week, <a href="https://forms.gle/gLH71FExmKwcJtp68"><u>register for an upcoming webinar</u>. </a>
+        If you are interested in applying to host an event or volunteer,
+        fill out our <a href="https://airtable.com/appzyMC4CJl7GwcK5/shrRXtM6RidqKM6qu"> <u>call for participation form</u>!</a>
         </p>
 
 
